ee9c1ebc4de53ea1e2abbf15981e937dadbc0aea
Mara <mrv006@hotmail.com>
2020-09-15 19:48:36 -0300

    vega

---
+++ include/footer.php
@@ -0,0 +1,9 @@
+<div class="footer mt-2 p-3">
+  <ul class="ulFooter">
+    <li class="liFooter" >Ayuda</li>
+    <li class="liFooter">Contacto</li>
+    <li class="liFooter">Terminos y condiciones</li>
+    <li class="liFooter">Politicas de Privacidad</li>
+
+  </ul>
+</div>
---
+++ include/nav.php
@@ -0,0 +1,43 @@
+<!-- Nav max -->
+<div class="NavMax pt-2">
+  <p class="mb-0 titulo_principal"><img src="img/logo.png" alt="" width="170"></p>
+  <div class="divIconosNav pr-5">
+    <i class="fas fa-search"></i>
+    <i class="fas fa-user-alt"></i>
+    <i class="fas fa-shopping-cart"></i>
+  </div>
+</div>
+
+
+<!-- Nav Reponsive -->
+<nav class="navbar navbar-expand-md navbar-light bg-light NavResponsive pt-4">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars" style="color: white;"></i>
+  </button>
+  <p class="titulo_responsive mb-0"> <img src="img/logo.png" alt="" width="170"> </p>
+  <i class="imgShopResponsive fas fa-shopping-cart"></i>
+
+
+<!-- Categorias -->
+
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="ulNav">
+      <li class="nav-item  active pr-3 pl-3">
+        <a class="nav-link liNav" href="#" style="font-weight: 600;">SALE</a>
+      </li>
+      <li class="nav-item  pr-3 pl-3">
+        <a class="nav-link liNav" href="#">E-SHOP</a>
+      </li>
+      <li class="nav-item  pr-3 pl-3">
+        <a class="nav-link liNav" href="#">LOOKBOOK</a>
+      </li>
+      <li class="nav-item  pr-3 pl-3">
+        <a class="nav-link liNav" href="#">CAMPA&Ntilde;A</a>
+      </li>
+      <li class="nav-item  pr-3 pl-3">
+        <a class="nav-link liNav" href="#">LOCALES</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
