60c50bed4aff2cd4bbb270123b51d85bdcd0fe79
nmolina.95 <ignaciomolina.95@hotmail.com>
2020-09-16 20:54:56 -0300

    Ahora se obtiene los productos por un JSON externo (vía API).

--- public/js/js.js
+++ public/js/js.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+
     let menu = document.querySelector('.menu-toggle');
     let navbar = document.querySelector('.navbar');
     let category = document.querySelector('.category-title');
@@ -7,14 +8,39 @@ window.onload = function () {
     let categoryList = document.querySelector('.category');
     let waistList = document.querySelector('.waist');
     let colourList = document.querySelector('.colour');
-    
 
 
+    /*====== Products from external JSON ======*/
+    fetch('https://gzevallos-mp-ecommerce-nodejs.herokuapp.com/api/products')
+        .then(response => response.json() )
+        .then(data => {
+            data.forEach(element => {
+                document.querySelector('.products-wrapper').innerHTML += `
+                    <div class="col-12 col-lg-4 ${element.type}">
+                        <section class="product-box">
+                            <figure class="product-box_image">
+                                <img src="public/img/${element.image}" alt="${element.name}">
+                            </figure>
+                            <article class="product-box_data">
+                                <span>${element.name}</span>
+                                <p>$${element.price}</p>
+                            </article>
+                        </section>
+                    </div>
+
+                `
+            });
+        })
+    /*====== /Products from external JSON ======*/
+
+    /*====== Mobile menu ======*/
     menu.addEventListener('click', e => {
         navbar.classList.toggle('navbar')
         navbar.classList.toggle('navbar-on')
     })
+    /*====== /Mobile menu ======*/
 
+    /*====== Filters ======*/
     category.addEventListener('click', e => {
     
         categoryList.classList.toggle('reduce');
@@ -51,6 +77,6 @@ window.onload = function () {
             colour.innerHTML = "-";
         }
     })
-
+    /*====== /Filters ======*/
 
 }
