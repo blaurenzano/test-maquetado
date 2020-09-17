be142abe82c2be2d8bb6029085ca1f4a5ba56d05
nmolina.95 <ignaciomolina.95@hotmail.com>
2020-09-16 23:28:23 -0300

    Se agregó un botón para quitar filtros activos, y se realizarón los últimos ajustes. Actualizado el README.

--- public/js/js.js
+++ public/js/js.js
@@ -31,19 +31,23 @@ window.onload = function () {
                 `
             });
 
+            /*====== Products filters ======*/
             let sneakers = document.querySelectorAll('.Zapatillas');
             let boots = document.querySelectorAll('.Botas');
             let shoes = document.querySelectorAll('.Zapatos');
             let sneakersList = document.querySelector('.sneakers-list');
             let bootsList = document.querySelector('.boots-list');
             let shoesList = document.querySelector('.shoes-list');
+            let quitFilter = document.querySelector('.quitFilter');
 
-            /* Products filters */
+            
             sneakersList.addEventListener('click', e => {
 
                 sneakersList.classList.add('bold');
                 bootsList.classList.remove('bold');
                 shoesList.classList.remove('bold');
+
+                quitFilter.classList.remove('reduce');
 
                 sneakers.forEach(y => {
                     y.classList.remove('reduce')
@@ -62,6 +66,8 @@ window.onload = function () {
                 bootsList.classList.add('bold');
                 shoesList.classList.remove('bold');
 
+                quitFilter.classList.remove('reduce');
+
                 boots.forEach(y => {
                     y.classList.remove('reduce')
                 })
@@ -79,6 +85,8 @@ window.onload = function () {
                 bootsList.classList.remove('bold');
                 shoesList.classList.add('bold');
 
+                quitFilter.classList.remove('reduce');
+
                 shoes.forEach(y => {
                     y.classList.remove('reduce')
                 })
@@ -88,6 +96,27 @@ window.onload = function () {
                 boots.forEach(y => {
                     y.classList.add('reduce')
                 })
+            })
+
+            quitFilter.addEventListener('click', e => {
+
+                sneakersList.classList.remove('bold');
+                bootsList.classList.remove('bold');
+                shoesList.classList.remove('bold');
+
+                quitFilter.classList.add('reduce');
+
+
+                shoes.forEach(y => {
+                    y.classList.remove('reduce')
+                })
+                sneakers.forEach(y => {
+                    y.classList.remove('reduce')
+                })
+                boots.forEach(y => {
+                    y.classList.remove('reduce')
+                })
+
             })
             /*====== /Products filters ======*/
             
