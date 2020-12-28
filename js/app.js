a7dc34fe47246721c77c3a6129e14af2d7f62e56
Omar Dario <melendrezomar2@gmail.com>
2020-12-28 09:05:54 -0300

    agregando funcionalidad de indicar columnas y realizando carga de datos de productos atravez de un archivo json

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+// -------------------PETICION-DE-DATOS-JSON--------------------
+function traerDatos() {
+  //   console.log('dentro de funcion')
+
+  const xhttp = new XMLHttpRequest()
+
+  xhttp.open('GET', '../json/productos.json', true)
+  xhttp.send()
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      //   console.log(this.responseText)
+      let datos = JSON.parse(this.responseText)
+      console.log(datos)
+
+      let res = document.querySelector('#grid-product')
+      res.innerHTML = ''
+
+      datos.forEach((producto) => {
+        res.innerHTML += `
+        <article class="card tres-seis">
+        <figure class="card__figure">
+            <img src="${producto.img}" alt="producto" class="card__image">
+        </figure>
+        <div class="card__body">
+            <div class="card__title">${producto.title}</div>
+            <div class="card__price">${producto.price}</div>
+        </div>
+        </article>
+        `
+      })
+    }
+  }
+}
+// -------------------END-PETICION-DE-DATOS-JSON----------------
+
 // -------------------MENU-MOBILE--------------------
 const mainMenu = document.querySelector('.mobile-menu')
 const closeMenu = document.querySelector('.mobile-menu__close')
@@ -31,3 +67,47 @@ for (let i = 0; i < btnCategoryFilter.length; i++) {
   })
 }
 // -------------------END-CATEGORY-WRAP--------------------
+
+// -------------------COLUMNS-FILTERS----------------------
+const oneColumn = document.getElementById('one')
+const twoColumn = document.getElementById('two')
+const threeColumn = document.getElementById('three')
+const fourColumn = document.getElementById('four')
+
+const grid = document.getElementById('grid-product')
+
+oneColumn.addEventListener('click', showOne)
+twoColumn.addEventListener('click', showTwo)
+threeColumn.addEventListener('click', showThree)
+fourColumn.addEventListener('click', showFour)
+
+function showOne() {
+  if (grid.style.gridTemplateColumns != 'repeat(1, 260px)') {
+    grid.style.gridTemplateColumns = 'repeat(1, 260px)'
+  } else {
+    grid.style.gridTemplateColumns = 'repeat(3, 260px)'
+  }
+}
+function showTwo() {
+  if (grid.style.gridTemplateColumns != 'repeat(2, 260px)') {
+    grid.style.gridTemplateColumns = 'repeat(2, 260px)'
+  } else {
+    grid.style.gridTemplateColumns = 'repeat(3, 260px)'
+  }
+}
+function showThree() {
+  if (grid.style.gridTemplateColumns != 'repeat(3, 260px)') {
+    grid.style.gridTemplateColumns = 'repeat(3, 260px)'
+  } else {
+    grid.style.gridTemplateColumns = 'repeat(3, 260px)'
+  }
+}
+function showFour() {
+  if (grid.style.gridTemplateColumns != 'repeat(4, 200px)') {
+    grid.style.gridTemplateColumns = 'repeat(4, 200px)'
+  } else {
+    grid.style.gridTemplateColumns = 'repeat(3, 260px)'
+  }
+}
+
+// -------------------END-COLUMNS-FILTERS------------------
